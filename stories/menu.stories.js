1449d33470c60ec12c139ec9b523092f9a920b64
Niels Wijers <nielswijers@gmail.com>
2019-11-14 13:25:41 +0100

    Initial setup

---
+++ stories/menu.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Menu } from '../packages/menu/src/testUtils';
+import './menu.stories.css';
+
+export default {
+  title: 'useMenu',
+};
+
+export const example = () => {
+  return <Menu />;
+};
